8bbfe42fa750707d3ca0a9a3079652ac21192de2
ankurcha10 <ankurcha10@gmail.com>
2024-08-03 16:05:39 +0530

    Routing Setup and Headet

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header"
+
+
+const Login = () => {
+  return (
+    <div>
+    <Header/>
+    <div>
+        <img
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/826348c2-cdcb-42a0-bc11-a788478ba5a2/f5a613af-ff99-444d-8305-e4cecd6d6cf6/US-en-20240729-POP_SIGNUP_TWO_WEEKS-perspective_WEB_591dffe8-33f4-4fb4-a734-9ff362a96145_large.jpg"alt="logo"
+        />
+    </div>
+
+    </div>
+  )
+}
+
+export default Login
